ab74ce12ceff87dea07c7acd5d7fed8a89950eea
John <johnlu@gliacloud.com>
2026-02-11 16:38:34 +0800

    Fix formatting in Package.swift by removing redundant platform declaration

--- Package.swift
+++ Package.swift
@@ -7,11 +7,7 @@ let package = Package(
     name: "GliaPlayer",
     platforms: [
         .iOS(.v14) // Minimum iOS version required
-    ],
-    platforms: [
-        .iOS(.v14) // Minimum iOS version required
-    ],
-    
+    ],    
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
